bae694c6a7949555e6cd5344171cd184e39c9e8e
Peter Gu <petergu@microsoft.com>
2017-11-16 20:09:57 +0800

    Initial version.

---
+++ StartMyVM/StartMyVM/StartMyVM.ps1
@@ -0,0 +1,24 @@
+#
+# StartMyVM.ps1 - continue retry until the VM get started. Please run Login-AzureRmVM first.
+#
+Param(
+    [Parameter(Mandatory=$True)][string]$ResourceGroupName,
+    [Parameter(Mandatory=$True)][string]$Name
+)
+
+$i = 1
+
+while ($status -ne "VM running")
+{
+    $vm = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $Name -Status
+    $status = ($vm.Statuses | ?{$_.Code -like "PowerState*"}).DisplayStatus
+
+    if ($status -ne "VM running")
+    {
+        $timestamp = ('[' + (Get-Date -Format hh:mm:ss.ff) + '] ')
+        Write-Host ($timestamp + "Trying to start the VM " + $Name + ". Number of attempts: " + $i)
+        $i++
+        $vm | Start-AzureRmVM
+        sleep -Seconds 5
+    }
+}
